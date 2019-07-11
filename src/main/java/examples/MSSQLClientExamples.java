156dd065593b32fa3ee2e1204eab71a0ced74e72
Billy Yuan <billy112487983@gmail.com>
2019-07-11 11:22:57 +0800

    initial work for documentation
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

---
+++ src/main/java/examples/MSSQLClientExamples.java
@@ -0,0 +1,12 @@
+package examples;
+
+import io.vertx.docgen.Source;
+
+@Source
+public class MSSQLClientExamples {
+  public void gettingStarted() {
+    throw new UnsupportedOperationException();
+  }
+
+
+}
